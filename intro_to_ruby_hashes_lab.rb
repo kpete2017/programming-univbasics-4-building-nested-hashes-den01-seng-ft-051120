d9c24da8797efa10b8e56e141d24b7de24bfad0a
Kyle Petersen <kpete2017@gmail.com>
2020-04-16 21:55:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,8 +6,8 @@ def monopoly_with_second_tier
   new_hash = { 
     :railroads => {
       :pieces => "4",
-      :rent_in_dollars = {},
-      :names = {}
+      :rent_in_dollars => {},
+      :names => {}
     } 
   }
 end
