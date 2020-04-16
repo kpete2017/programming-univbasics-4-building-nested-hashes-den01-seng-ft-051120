3e10944ad14fc454576bd8f19f418fe47ee2dfd5
Kyle Petersen <kpete2017@gmail.com>
2020-04-16 21:55:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,12 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  new_hash = { :railroads => {} }
+  new_hash = { 
+    :railroads => {
+      :pieces => "4",
+      :rent_in_dollars = {},
+      :names = {}
+    } }
 end
 
 def monopoly_with_third_tier
